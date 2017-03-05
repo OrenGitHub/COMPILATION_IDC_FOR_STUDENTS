13b063fa591ffa1fc3b248f04de3e55f3a02f103
OrenGitHub <oren@oren>
2017-03-05 11:47:56 +0200

    ...

---
+++ FOLDER_2_SOURCE_CODE/EX3/LINUX_GCC_MAKE/FOLDER_06_Lini_HEADER_FILES/FILE_18_SolutionSet_API.h
@@ -0,0 +1,24 @@
+#ifndef __MAT_READER_API_H__
+#define __MAT_READER_API_H__
+
+/**************************************/
+/* FILE NAME: FILE_08_MatReader_API.h */
+/**************************************/
+
+/*************************/
+/* PROJECT INCLUDE FILES */
+/*************************/
+#include "FILE_03_Matrix.h"
+
+/*************/
+/* FUNCTIONS */
+/*************/
+Matrix Parse_Input_Matrix(string Matrix_Filename);
+
+//extern int yyparse(void);
+//extern A_exp absyn_root;
+
+//extern int zzparse(void);
+//extern PSEUDO_MIPS_ASM_AST_expList PSEUDO_MIPS_ASM_AST_root;
+
+#endif
