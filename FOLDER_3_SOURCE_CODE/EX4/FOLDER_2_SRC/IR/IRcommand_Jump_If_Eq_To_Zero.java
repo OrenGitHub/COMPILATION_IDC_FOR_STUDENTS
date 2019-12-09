539a53358e04376aa73b922ce587ad02555b64bf
OrenGitHub <tuna.is.good.for.you@gmail.com>
2019-12-09 08:37:14 +0200

    EX4

---
+++ FOLDER_3_SOURCE_CODE/EX4/FOLDER_2_SRC/IR/IRcommand_Jump_If_Eq_To_Zero.java
@@ -0,0 +1,43 @@
+/***********/
+/* PACKAGE */
+/***********/
+package IR;
+
+/*******************/
+/* GENERAL IMPORTS */
+/*******************/
+
+/*******************/
+/* PROJECT IMPORTS */
+/*******************/
+import TEMP.*;
+import LLVM.*;
+import MIPS.*;
+
+public class IRcommand_Jump_If_Eq_To_Zero extends IRcommand
+{
+	TEMP t;
+	String label_name;
+	
+	public IRcommand_Jump_If_Eq_To_Zero(TEMP t, String label_name)
+	{
+		this.t          = t;
+		this.label_name = label_name;
+	}
+
+	/*******************/
+	/* LLVM bitcode me */
+	/*******************/
+	public void LLVM_bitcode_me()
+	{
+		LLVM.getInstance().beqz(t,label_name);
+	}
+	
+	/***************/
+	/* MIPS me !!! */
+	/***************/
+	public void MIPSme()
+	{
+		sir_MIPS_a_lot.getInstance().beqz(t,label_name);
+	}
+}
